b026fc4561a7cc0744cd7c823a9991317b6c0e6e
Stefano De Marchi <102753287+demarchi17@users.noreply.github.com>
2022-04-19 06:58:46 +0200

    Update AldazBernstein1d2d.m

--- AldazBernstein1d2d.m
+++ AldazBernstein1d2d.m
@@ -3,19 +3,31 @@ close all
 
 %---------------------------------------------------------
 % Copyright by Ana Maria Acu and Stefano De Marchi
-% March 2022
+% April 2022
 %
-%  This script compute the Aldaz and Bernstein approximation
-%  of a function in 1d and 2d as well as cubature
+%  This script computes the Aldaz-Kouncev-Render 
+%  and Bernstein approximating operators
+%  of a function in [0,1] and in [0,1]^2
+%
+% Reference
+% A. Acu, S. De Marchi and I. Rasa
+% Aldaz-Kounchev-Render operators and their approximation properties 
+% (submitted April 2022)
 %---------------------------------------------------------
 
-% n=input('give me n=');
-% j=input('give me j (>=1)');
-j=2;
-m=100; N=20;
+ N=input('give me N=');
+ j=input('give me j (>=1)');
+%j=2; 
+%N=4;    %%%%%%% Put N=3 for examples 4.2 and 4.3
+
+M=50;   %nr. evaluation points
+
+disp('1 dimensional example')
+
 f1d=@(x) -2/pi*x.*cos(pi*x/2)+4/pi^2*sin(pi*x/2); %function to approximate
-x=linspace(0,1,m);   %evaluation points
+x=linspace(0,1,M);   %evaluation points
 y=x;
+
 
 for n=j:N
 
@@ -42,38 +54,67 @@ for i=1:length(x),
 end
 
 
-plot(x,f1d(x),'r', x,bbA,'b', x,bbB,'k');
-
 ff=f1d(x);
 eA(n)=norm(ff-bbA,inf)/norm(ff);
 eB(n)=norm(ff-bbB,inf)/norm(ff);
 end
 
-semilogy(1:length(eA),eA,'r', 1:length(eB), eB, 'b-.')
+figure
+plot(x,f1d(x),'r-',  x,bbB,'k--',x,bbA,'b-.');
+legend('function','B-Approx','AKR-approx');
+
+figure
+semilogy(1:length(eA),eA,'b--', 1:length(eB), eB, 'k-.')
 legend('relerr 1d AKR','relerr 1d Bernstein')
 grid
 
 
 clear n
-%
+%-------------------------
 %  bivariate example
-%
-f2d=@(x,y) exp(x.^2.*y.^2)-1;
+%-------------------------
+disp('2 dimensional example')
+ExampleNr=input('Example 4.2 type 2, Example 4.3 type 3, Example 4.4 type 4 : ')
+
+
 
 for n=j:N
-% figure(2)
+ if ExampleNr==2,
+    m=n+1;
+    f2d=@(x,y) exp(x.^2.*y.^2)-1;       %Example 4.2
+end
+ if ExampleNr==3
+    m=n+1;
+    f2d=@(x,y) tan(pi/4*(2.^(x.^j)-1).*y.^(3*j)); %Example 4.3
+ end
+ if ExampleNr==4
+    m=n;
+   f2d=@(x,y) (1/pi)^4*(-64*pi*(y+x).*cos(pi/4*(y+x))+(256-16*pi^2*x.*y).*sin(pi/4*(x+y))+...
+   64*pi*x.*cos(pi*x/4)+64*pi*y.*cos(pi/4*y)-256*(sin(pi/4*x)+sin(pi/4*y))); %Example 4.4
+ end
+ if ExampleNr<1 | ExampleNr>4
+   error('2d example number not studied!')
+ end
+    
+    
 % Bernstein points
-b=0:1/n:1;
+bbx=0:1/n:1;
+bby=0:1/m:1;
 
 
-% Aldaz points
-a(1:j)=0;
+% Aldaz points along x
+aax(1:j)=0;
 for k=j:n
-    a(k+1)= (factorial(k)/factorial(k-j)*factorial(n-j)/factorial(n))^(1/j);
+    aax(k+1)= (factorial(k)/factorial(k-j)*factorial(n-j)/factorial(n))^(1/j);
 end
 
- [bx,by]=meshgrid(b);   %grid of Bernstein points
- [ax,ay]=meshgrid(a);   %grid of Aldaz points
+aay(1:j)=0;
+for k=j:m
+    aay(k+1)= (factorial(k)/factorial(k-j)*factorial(m-j)/factorial(m))^(1/j);
+end
+
+ [bx,by]=meshgrid(bbx,bby);   %grid of Bernstein points
+ [ax,ay]=meshgrid(aax,aay);   %grid of Aldaz points
  
  
  f2dB=f2d(bx,by);
@@ -86,8 +127,14 @@ end
   for k=0:n,
      Bx(i,k+1)=nchoosek(n,k)*w(i)^k*(1-w(i))^(n-k);
     end
+ end
+for i=1:length(w),
+  for k=0:m,
+     By(i,k+1)=nchoosek(m,k)*w(i)^k*(1-w(i))^(m-k);
+    end
 end
-By=Bx;
+ 
+%By=Bx;
  
  %Bx=bernsteinMatrix(n,w);
 
@@ -95,17 +142,36 @@ By=Bx;
  F=f2d(X,Y);
  %mesh(X,Y,F)
  
- BBernstein=Bx*f2dB*By';
- BAldaz=Bx*f2dA*By';
+ BBernstein=By*f2dB*Bx';
+ BAldaz=By*f2dA*Bx';
  
  errBer2d(n)=norm(BBernstein-F)/norm(F);
  errAKR2d(n)=norm(BAldaz-F)/norm(F);
 
-
-
 end
 
+% figure
+% semilogy(1:length(errBer2d),errBer2d,'b-.',1:length(errAKR2d),errAKR2d,'r')
+% legend('relerr Bernstein 2d','relerr AKR 2d')
+% grid
+
+subindex=[num2str(n),num2str(m),num2str(j)];
+AKRstr=['B_{',subindex,'}f-f'];
+Bstr=['B_{',num2str(n),num2str(m),'}f-f'];
+BAKRstr=['B_{',subindex,'}f-','B_{',num2str(n),num2str(m),'}f'];
+
 figure
-semilogy(1:length(errBer2d),errBer2d,'b-.',1:length(errAKR2d),errAKR2d,'r')
-legend('relerr Bernstein 2d','relerr AKR 2d')
-grid
+surfc(X,Y,BAldaz-F)
+
+title(AKRstr)
+view(-60,30)
+
+figure
+surfc(X,Y,BBernstein-F)
+title(Bstr)
+view(-60,30)
+
+figure
+surfc(X,Y,BBernstein-BAldaz)
+title(BAKRstr)
+view(-60,30)
